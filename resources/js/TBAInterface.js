9f6089ddc8c2cc6bc9454454c0260cefda11d83d
G <156869076+Gabriel-A-Gonzalez@users.noreply.github.com>
2026-02-21 10:25:35 -0700

    Update authKey for TheBlueAlliance API

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "N9qP6qaoBFOk0vwyb6KD49nnlnK6yO2M9YlhsHwLqX2gNm4N2Rx8MpKt94LGhOeZ";
 /**
  * Get list of teams in event
  *
